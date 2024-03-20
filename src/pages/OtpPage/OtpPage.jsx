915753f22a4e21808cca2a1d74affafebd1577d1
Shahrukh <shahrukh1494@gmail.com>
2024-03-20 19:25:34 +0530

    added account doesnt exist popup modal

--- src/pages/OtpPage/OtpPage.jsx
+++ src/pages/OtpPage/OtpPage.jsx
@@ -3,48 +3,59 @@ import "./otppage.scss";
 import Button from "../../components/Button/Button";
 import TitleText from "../../components/TitleText/TitleText";
 import OTPInput from "react-otp-input";
+import NoAccountModal from "../../components/NoAccountModal/NoAccountModal";
 
 const OtpPage = () => {
   const [otp, setOtp] = useState("");
   const [incorrectOtp, setIncorrectOtp] = useState(false);
+  const [showNoAccountPopup, setShowNoAccountPopup] = useState(false);
 
   return (
-    <div className="main_page">
-      <div className="main_page_content">
-        <TitleText title={"One time password"} />
-        <div className="main_page_desc">
-          We have sent OTP to your registered mobile number{" "}
-          <span className="otp-page-phone-number">+91 458 687 2222</span>
+    <>
+      <div className="main_page">
+        <div className="main_page_content">
+          <TitleText title={"One time password"} />
+          <div className="main_page_desc">
+            We have sent OTP to your registered mobile number{" "}
+            <span className="otp-page-phone-number">+91 458 687 2222</span>
+          </div>
+          <div className="otp-input-label">OTP</div>
+          <div className="otp-input-wrapper-div">
+            <OTPInput
+              value={otp}
+              onChange={setOtp}
+              isInputNum
+              numInputs={6}
+              pattern="[0-9]*"
+              inputType="number"
+              renderInput={(props) => (
+                <input
+                  {...props}
+                  className={
+                    incorrectOtp
+                      ? "custom_input_one border_error"
+                      : "custom_input_one"
+                  }
+                  type="text"
+                  inputMode="decimal"
+                />
+              )}
+            />
+          </div>
+          <div
+            className="resend-otp-link-txt"
+            onClick={() => setShowNoAccountPopup(true)}
+          >
+            Resend OTP
+          </div>
         </div>
-        <div className="otp-input-label">OTP</div>
-        <div className="otp-input-wrapper-div">
-          <OTPInput
-            value={otp}
-            onChange={setOtp}
-            isInputNum
-            numInputs={6}
-            pattern="[0-9]*"
-            inputType="number"
-            renderInput={(props) => (
-              <input
-                {...props}
-                className={
-                  incorrectOtp
-                    ? "custom_input_one border_error"
-                    : "custom_input_one"
-                }
-                type="text"
-                inputMode="decimal"
-              />
-            )}
-          />
-        </div>
+        <Button
+          onClickFunction={() => console.log("clicked")}
+          title={"Submit delete request"}
+        />
       </div>
-      <Button
-        onClickFunction={() => console.log("clicked")}
-        title={"Submit delete request"}
-      />
-    </div>
+      {showNoAccountPopup ? <NoAccountModal /> : null}
+    </>
   );
 };
 
