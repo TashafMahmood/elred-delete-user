386be1fb1868647c9f0cd2d6b86314e692ec9908
TashafMahmood <tashaf@elred.io>
2024-03-20 18:14:28 +0530

    success page

--- src/pages/OtpPage/OtpPage.jsx
+++ src/pages/OtpPage/OtpPage.jsx
@@ -3,48 +3,54 @@ import "./otppage.scss";
 import Button from "../../components/Button/Button";
 import TitleText from "../../components/TitleText/TitleText";
 import OTPInput from "react-otp-input";
+import SuccessPage from "../SuccessPage/SuccessPage";
 
 const OtpPage = () => {
   const [otp, setOtp] = useState("");
   const [incorrectOtp, setIncorrectOtp] = useState(false);
+  const [success, setSuccess] = useState(false)
 
   return (
-    <div className="main_page">
-      <div className="main_page_content">
-        <TitleText title={"One time password"} />
-        <div className="main_page_desc">
-          We have sent OTP to your registered mobile number{" "}
-          <span className="otp-page-phone-number">+91 458 687 2222</span>
-        </div>
-        <div className="otp-input-label">OTP</div>
-        <div className="otp-input-wrapper-div">
-          <OTPInput
-            value={otp}
-            onChange={setOtp}
-            isInputNum
-            numInputs={6}
-            pattern="[0-9]*"
-            inputType="number"
-            renderInput={(props) => (
-              <input
-                {...props}
-                className={
-                  incorrectOtp
-                    ? "custom_input_one border_error"
-                    : "custom_input_one"
-                }
-                type="text"
-                inputMode="decimal"
+    <>
+      {
+        success ? <SuccessPage /> : <div className="main_page">
+          <div className="main_page_content">
+            <TitleText title={"One time password"} />
+            <div className="main_page_desc">
+              We have sent OTP to your registered mobile number{" "}
+              <span className="otp-page-phone-number">+91 458 687 2222</span>
+            </div>
+            <div className="otp-input-label">OTP</div>
+            <div className="otp-input-wrapper-div">
+              <OTPInput
+                value={otp}
+                onChange={setOtp}
+                isInputNum
+                numInputs={6}
+                pattern="[0-9]*"
+                inputType="number"
+                renderInput={(props) => (
+                  <input
+                    {...props}
+                    className={
+                      incorrectOtp
+                        ? "custom_input_one border_error"
+                        : "custom_input_one"
+                    }
+                    type="text"
+                    inputMode="decimal"
+                  />
+                )}
               />
-            )}
+            </div>
+          </div>
+          <Button
+            onClickFunction={() => setSuccess(true)}
+            title={"Submit delete request"}
           />
         </div>
-      </div>
-      <Button
-        onClickFunction={() => console.log("clicked")}
-        title={"Submit delete request"}
-      />
-    </div>
+      }
+    </>
   );
 };
 
