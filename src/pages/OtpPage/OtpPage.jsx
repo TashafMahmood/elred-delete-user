707160262e0d9756f812368b53d706d6b032bf63
Shahrukh <shahrukh1494@gmail.com>
2024-03-21 11:24:09 +0530

    added resetotp timer UI

--- src/pages/OtpPage/OtpPage.jsx
+++ src/pages/OtpPage/OtpPage.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./otppage.scss";
 import Button from "../../components/Button/Button";
 import TitleText from "../../components/TitleText/TitleText";
 import OTPInput from "react-otp-input";
 import SuccessPage from "../SuccessPage/SuccessPage";
 import NoAccountModal from "../../components/NoAccountModal/NoAccountModal";
+import { useCountdownTimer } from "../../components/Hooks/useCountDownTimer";
 
 const OtpPage = ({ number }) => {
   const [otp, setOtp] = useState("");
   const [incorrectOtp, setIncorrectOtp] = useState(false);
   const [success, setSuccess] = useState(false);
   const [showNoAccountPopup, setShowNoAccountPopup] = useState(false);
+  const [startTimer, setStartTimer] = useState(false);
+  const { timer, formatTime, resetTimer } = useCountdownTimer(60, startTimer);
+
+  useEffect(() => {
+    setStartTimer(true);
+  }, []);
 
   return (
     <>
@@ -50,12 +57,21 @@ const OtpPage = ({ number }) => {
                 <div class="incorrect-otp-error">Invalid OTP entered</div>
               ) : null}
             </div>
-            <div
-              className="resend-otp-link-txt"
-              onClick={() => setShowNoAccountPopup(true)}
-            >
-              Resend OTP
-            </div>
+            {timer > 0 && startTimer ? (
+              <div className="otp-time-remaining">
+                Time Remaining: {timer > 0 && formatTime(timer)}
+              </div>
+            ) : (
+              <div className="resend-otp-link-container">
+                <span
+                  className="resend-otp-link-txt"
+                  onClick={() => setShowNoAccountPopup(true)}
+                  // onClick={resetTimer}
+                >
+                  Resend OTP
+                </span>
+              </div>
+            )}
           </div>
           <Button
             onClickFunction={() => setSuccess(true)}
